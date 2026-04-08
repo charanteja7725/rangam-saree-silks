c0b02e99dc1054fffe94710179c3965982ecd7e1
chathurima <chathurimajaldu@gmail.com>
2026-04-08 16:44:31 +0530

    added cartpage and day4 completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,6 +13,7 @@ export default function Navbar() {
           <Link to="/products">Products</Link>
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
+          <Link to="/cart">Cart</Link>
         </nav>
       </div>
     </header>
